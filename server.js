c441ea140d837c74e26a71e9745784c9882bbcba
Naman Kanakiya <naman.kanakiya@gmail.com>
2020-04-22 21:54:58 -0700

    change server to listen to port

--- server.js
+++ server.js
@@ -96,8 +96,8 @@ nextApp.prepare().then(() => {
     return nextHandler(req, res);
   });
 
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 });
