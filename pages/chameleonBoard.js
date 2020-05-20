6791cc9b22d83bcb8c23c1967605689a780e74cc
Naman Kanakiya <naman.kanakiya@gmail.com>
2020-05-20 15:57:46 -0700

    go back to non-fixed layout

--- pages/chameleonBoard.js
+++ pages/chameleonBoard.js
@@ -52,8 +52,6 @@ export default class ChameleonSelect extends Component {
               "> table": {
                 my: [2, 3],
                 textAlign: "center",
-                tableLayout: "fixed",
-                wordBreak: "break-word",
               },
               th: { color: "primary", textAlign: "center" },
               td: { mx: "20px" },
